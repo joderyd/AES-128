ab1853de4e2eea7921302be937e2b810dbcc3d30
Jonathan Öderyd <47157963+joderyd@users.noreply.github.com>
2019-05-20 14:48:26 +0200

    Update KeyManager.java

--- src/KeyManager.java
+++ src/KeyManager.java
@@ -1,17 +1,22 @@
+/*@
+Class for managing key expansion by calculating the next round key
+based on the previous key and key round
+ */
+
 class KeyManager {
     private static final int[] roundConstants = { 1, 2, 4, 8, 16, 32, 64, 128, 27, 54 };
 
 
     static int[][] getNextKey(int[][] prevKey, int keyRound){
         int[][] nextKey = new int[4][4];
-        int[] rcon = new int[]{roundConstants[keyRound], 0, 0, 0};
+        int[] rConstants = new int[]{roundConstants[keyRound], 0, 0, 0};
 
         int[] col0 = AESutils.getColumn(prevKey, 0);
         int[] col3 = RotWord(AESutils.getColumn(prevKey, 3));
-        int[] w4 = SubBytes(col3, AESutils.getSbox());
-
+        int[] w4 = SubBytes(col3, AES.sBox);
+        // bitwise xOr
         for(int i=0; i<4; i++){
-            nextKey[i][0] = w4[i] ^ rcon[i] ^ col0[i];
+            nextKey[i][0] = w4[i] ^ rConstants[i] ^ col0[i];
         }
         for(int r=0; r<4; r++){
             for(int c=1; c<4; c++){
@@ -53,7 +58,6 @@ class KeyManager {
                 int row = Integer.parseInt(b1, 16);
                 int col = Integer.parseInt(b2, 16);
                 word[i] = sBox[row][col];
-                //word[i] = sBox[hexVal/16][hexVal%16];
         }
         return word;
     }
